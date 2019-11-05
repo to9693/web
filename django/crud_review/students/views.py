422e9b36f5134a1eed94f2641504db6dff288462
to9693 <rkdxh321@jbnu.ac.kr>
2019-11-05 15:06:40 +0900

    191105 admin comment_crud

--- django/crud_review/students/views.py
+++ django/crud_review/students/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .models import Student
+from .models import Student, Comment
 
 # Create your views here.
 def index(request):
@@ -93,7 +93,22 @@ def edit(request, pk):
         return render(request, 'students/edit.html', context)
    
 
-# POST 요청만을 받음
+# POST 요청만을 받음 -> redirect()
+# def update(request, pk):
+    # # 1. pk에 해당하는 student를 DB에서 가져오기
+    # student = Student.objects.get(pk = pk)
+
+    # # 2. POST 요청을 통해 넘어온 데이터 가져오기
+    # name = request.POST.get('name')
+    # age = reqeust.POST.get('age')
+
+    # # 3. student 인스턴스의 정보를 변경 & DB에 반영 -> .save()
+    # student.name = name
+    # student.age = age
+    # student.save()
+
+    # # 4. student 상세 페이지로 이동(Detail)
+    # return redirect('student:detail', student.pk)
 
    
 
@@ -102,10 +117,51 @@ def delete(request, pk):
     if request.method =='POST':
 
         # 1. pk에 해당하는 학생을 DB에서 가져옴
-        student = Student.objects.get(pk = pk)
-
+        student = Student.objects.get(pk = pk) 
+ 
         # 2. student 삭제(DB에서 삭제하기))
         student.delete()
 
         # 3. index  페이지로 이동
         return redirect('students:index')
+
+
+# POST 요청을 받음 => redirect
+def comments_new(request, student_pk):
+    # 1. request에서 데이터 가져오기
+    content = request.POST.get('content')
+    
+    # 2. Comment 생성
+    comment = Comment()
+    comment.content = content
+    comment.student_id = student_pk
+    comment.save()
+
+    # 3. student 상세 페이지로 redirect
+    return redirect('students:detail', student_pk)
+
+# POST 요청을 받음
+def comments_delete(reqeust, student_pk ,pk):
+    comment = Comment.objects.get(pk=pk)
+    comment.delete()
+    return redirect('studentsLdetail', student_pk)
+
+def comments_edit(request, student_pk, pk):
+    comment = Comment.objects.get(pk=pk)
+
+    if request.method == 'PSOT':
+        # POST
+        # 1. POST로 넘어온 데이터 가져오기
+        content = request.POST.get('content')
+
+        # 2. comment에 바꿔 넣기 & 저장
+        comment.content = content
+        comment.save()
+        return redirect('students:detail', student_pk)
+
+    else:
+        # GET
+        context = {
+            'comment' :comment,
+        }
+        return render(request, 'students/comments_edit.html', context)
