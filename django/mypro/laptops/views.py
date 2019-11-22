a1d56da71f9839e6dac87059136296080ec42565
to9693 <rkdxh321@jbnu.ac.kr>
2019-11-22 16:14:22 +0900

    191122 mypro

--- django/mypro/laptops/views.py
+++ django/mypro/laptops/views.py
@@ -3,23 +3,19 @@ from .models import Laptop
 from django.core.paginator import Paginator
 # Create your views here.
 def index(request):
-    laptop_list = Laptop.objects.all()
-
-    # Pagination
-    # 1. arrticles를 Paginator에 입력
-    paginator = Paginator(laptop_list,20)
-    # 2. 몇번째 page를 보여줄건지 GET으로 가져옴
-    page = request.GET.get('page')
-    # 3. 해당하는 page의 articles만 뽑기
-    laptops = paginator.get_page(page)
+ 
+    return render(request, 'laptops/index.html')
     
+
+
+# GET 요청을 받음
+def search(request):
+    # 1. request로부터 검색어 가져오기
+    query = request.GET.get('query') #=> 'asdf'
+    # 2. Article에서 제목에 검색어가 있는지 찾기
+    laptops = Laptop.objects.filter(spec__contains = query)
+    # 3. context로 결과값 template에 넘겨주기
     context = {
         'laptops' : laptops,
     }
-
-    return render(request, 'laptops/index.html', context)
-    
-
-
-def search(request):
-    return render(request, 'laptops/search.html')
+    return render(request, 'laptops/search.html', context)
