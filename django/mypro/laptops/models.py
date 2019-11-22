8ca8c5a1a586d5c8692b9ece7ec37d15e1b2ce22
to9693 <rkdxh321@jbnu.ac.kr>
2019-11-22 18:57:35 +0900

    191122 dataset

--- django/mypro/laptops/models.py
+++ django/mypro/laptops/models.py
@@ -5,3 +5,11 @@ class Laptop(models.Model):
     name = models.CharField(max_length = 30)
     spec = models.TextField()
     price = models.CharField(max_length = 30)
+
+
+
+class Program(models.Model):
+    name = models.CharFeild(max_length = 20)
+    spec = models.CharFeild(max_length = 30)
+
+    laptops = models.ManyToManyField(Laptop, related_name = 'laptops')
--- django/mypro/laptops/views.py
+++ django/mypro/laptops/views.py
@@ -9,13 +9,18 @@ def index(request):
 
 
 # GET 요청을 받음
+# def search(request):
+#     # 1. request로부터 검색어 가져오기
+#     query = request.GET.get('query') #=> 'asdf'
+#     # 2. Article에서 제목에 검색어가 있는지 찾기
+#     laptops = Laptop.objects.filter(spec__contains = query)
+#     # 3. context로 결과값 template에 넘겨주기
+#     context = {
+#         'laptops' : laptops,
+#     }
+#     return render(request, 'laptops/search.html', context)
+
 def search(request):
     # 1. request로부터 검색어 가져오기
-    query = request.GET.get('query') #=> 'asdf'
-    # 2. Article에서 제목에 검색어가 있는지 찾기
-    laptops = Laptop.objects.filter(spec__contains = query)
-    # 3. context로 결과값 template에 넘겨주기
-    context = {
-        'laptops' : laptops,
-    }
-    return render(request, 'laptops/search.html', context)
+    query = request.GET.get('query')
+    
